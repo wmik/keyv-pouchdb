d974ca8d7d61792bf898272924434e3a424b13d9
wmik <williemik.wmik@gmail.com>
2018-06-28 09:34:14 +0300

    Fix indentation

--- src/index.js
+++ src/index.js
@@ -7,103 +7,103 @@ const merge = require("lodash.merge");
 PouchDB.plugin(require("pouchdb-lru-cache"));
 
 function dbFactory(opts) {
-  const adapters = {
-    http: "pouchdb-adapter-http",
-    memory: "pouchdb-adapter-memory",
-    websql: "pouchdb-adapter-node-websql"
-  };
+	const adapters = {
+		http: "pouchdb-adapter-http",
+		memory: "pouchdb-adapter-memory",
+		websql: "pouchdb-adapter-node-websql"
+	};
 
-  if (!Object.prototype.hasOwnProperty.call(adapters, opts.pouchDB.adapter)) {
-    throw new Error(`Unsupported pouchdb adapter ${opts.pouchDB.adapter}`);
-  }
+	if (!Object.prototype.hasOwnProperty.call(adapters, opts.pouchDB.adapter)) {
+		throw new Error(`Unsupported pouchdb adapter ${opts.pouchDB.adapter}`);
+	}
 
-  PouchDB.plugin(require(adapters[opts.pouchDB.adapter]));
+	PouchDB.plugin(require(adapters[opts.pouchDB.adapter]));
 
-  const db = new PouchDB(
-    opts.pouchDB.database,
-    Object.assign(
-      {},
-      opts.pouchDB.adapter === "http"
-        ? opts.pouchDB.remoteConfig
-        : { adapter: opts.pouchDB.adapter }
-    )
-  );
-  db.initLru(opts.maxCacheSize);
+	const db = new PouchDB(
+		opts.pouchDB.database,
+		Object.assign(
+			{},
+			opts.pouchDB.adapter === "http"
+				? opts.pouchDB.remoteConfig
+				: { adapter: opts.pouchDB.adapter }
+		)
+	);
+	db.initLru(opts.maxCacheSize);
 
-  return db.lru;
+	return db.lru;
 }
 
 class KeyvPouchdb extends EventEmitter {
-  constructor(opts, config = {}) {
-    super();
-    if (typeof opts === "string") {
-      if (opts.endsWith(".db")) {
-        opts = { pouchDB: { database: opts, adapter: "websql" } };
-      } else {
-        opts = {
-          pouchDB: {
-            database: opts,
-            adapter: "http",
-            remoteConfig: Object.assign({}, config)
-          }
-        };
-      }
-    }
-    this._opts = merge(
-      {
-        pouchDB: {
-          adapter: "memory",
-          database: "keyv-pouchdb-cache",
-          remoteConfig: {}
-        },
-        maxCacheSize: 5000000, // 5MB
-        overwriteExisting: false
-      },
-      opts
-    );
-    this.ttlSupport = false;
-    this.pouchdb = dbFactory(this._opts);
-  }
+	constructor(opts, config = {}) {
+		super();
+		if (typeof opts === "string") {
+			if (opts.endsWith(".db")) {
+				opts = { pouchDB: { database: opts, adapter: "websql" } };
+			} else {
+				opts = {
+					pouchDB: {
+						database: opts,
+						adapter: "http",
+						remoteConfig: Object.assign({}, config)
+					}
+				};
+			}
+		}
+		this._opts = merge(
+			{
+				pouchDB: {
+					adapter: "memory",
+					database: "keyv-pouchdb-cache",
+					remoteConfig: {}
+				},
+				maxCacheSize: 5000000, // 5MB
+				overwriteExisting: false
+			},
+			opts
+		);
+		this.ttlSupport = false;
+		this.pouchdb = dbFactory(this._opts);
+	}
 
-  get(key) {
-    return this.pouchdb
-      .get(key)
-      .then(value => Buffer.from(value).toString("binary"))
-      .catch(err => (err.status === 404 ? undefined : err)); // for api compliance: resolve undefined with nonexistent keys
-  }
+	get(key) {
+		return this.pouchdb
+			.get(key)
+			.then(value => Buffer.from(value).toString("binary"))
+			.catch(err => (err.status === 404 ? undefined : err)); // for api compliance: resolve undefined with nonexistent keys
+	}
 
-  set(key, value, ttl, type = "text/plain") {
-    return this.pouchdb
-      .has(key)
-      .then(hasIt => {
-        // pouchdb-lru-cache doesn't overwrite existing keys
-        if (hasIt && this._opts.overwriteExisting) {
-          return this.pouchdb.del(key);
-        }
-        return { ok: !hasIt };
-      })
-      .then(
-        write => write.ok && this.pouchdb.put(key, Buffer.from(value), type)
-      )
-      .then(response => (response ? response.ok : undefined));
-  }
+	set(key, value, ttl, type = "text/plain") {
+		return this.pouchdb
+			.has(key)
+			.then(hasIt => {
+				// pouchdb-lru-cache doesn't overwrite existing keys
+				if (hasIt && this._opts.overwriteExisting) {
+					return this.pouchdb.del(key);
+				}
+				return { ok: !hasIt };
+			})
+			.then(
+				write => write.ok && this.pouchdb.put(key, Buffer.from(value), type)
+			)
+			.then(response => (response ? response.ok : undefined));
+	}
 
-  delete(key) {
-    return this.pouchdb
-      .has(key)
-      .then(hasIt => (hasIt ? this.pouchdb.del(key) : { ok: false }))
-      .then(deleted => deleted.ok);
-  }
+	delete(key) {
+		return this.pouchdb
+			.has(key)
+			.then(hasIt => (hasIt ? this.pouchdb.del(key) : { ok: false }))
+			.then(deleted => deleted.ok);
+	}
 
-  clear() {
-    const pouchdb = new PouchDB(this._opts.pouchDB.database, {
-      [this._opts.pouchDB.adapter !== "http" && "adapter"]: this._opts.pouchDB
-        .adapter
-    });
-    return pouchdb.destroy().then(() => {
-      this.pouchdb = dbFactory(this._opts);
-    });
-  }
+	clear() {
+		const pouchdb = new PouchDB(this._opts.pouchDB.database, {
+			[this._opts.pouchDB.adapter !== "http" && "adapter"]: this._opts.pouchDB
+				.adapter
+		});
+		return pouchdb.destroy().then(() => {
+			this.pouchdb = dbFactory(this._opts);
+		});
+	}
 }
 
 module.exports = KeyvPouchdb;
--- test/test.js
+++ test/test.js
@@ -4,149 +4,149 @@ import KeyvPouchDB from "this";
 import { keyvValueTests } from "@keyv/test-suite";
 
 const store = (
-  opts = {
-    overwriteExisting: true
-  }
+	opts = {
+		overwriteExisting: true
+	}
 ) => new KeyvPouchDB(opts);
 
 const customizedkeyvApiTests = (test, Keyv, store) => {
-  test.beforeEach(async () => {
-    const keyv = new Keyv({ store: store() });
-    await keyv.clear();
-  });
+	test.beforeEach(async () => {
+		const keyv = new Keyv({ store: store() });
+		await keyv.clear();
+	});
 
-  test.serial(".set(key, value) resolves to true", async t => {
-    const keyv = new Keyv({ store: store() });
-    t.is(await keyv.set("foo", "bar"), true);
-  });
+	test.serial(".set(key, value) resolves to true", async t => {
+		const keyv = new Keyv({ store: store() });
+		t.is(await keyv.set("foo", "bar"), true);
+	});
 
-  test.serial(".set(key, value) sets a value", async t => {
-    const keyv = new Keyv({ store: store() });
-    await keyv.set("foo", "bar");
-    t.is(await keyv.get("foo"), "bar");
-  });
+	test.serial(".set(key, value) sets a value", async t => {
+		const keyv = new Keyv({ store: store() });
+		await keyv.set("foo", "bar");
+		t.is(await keyv.get("foo"), "bar");
+	});
 
-  test.serial(".get(key) resolves to value", async t => {
-    const keyv = new Keyv({ store: store() });
-    await keyv.set("foo", "bar");
-    t.is(await keyv.get("foo"), "bar");
-  });
+	test.serial(".get(key) resolves to value", async t => {
+		const keyv = new Keyv({ store: store() });
+		await keyv.set("foo", "bar");
+		t.is(await keyv.get("foo"), "bar");
+	});
 
-  test.serial(
-    ".get(key) with nonexistent key resolves to undefined",
-    async t => {
-      const keyv = new Keyv({ store: store() });
-      t.is(await keyv.get("foo"), undefined);
-    }
-  );
+	test.serial(
+		".get(key) with nonexistent key resolves to undefined",
+		async t => {
+			const keyv = new Keyv({ store: store() });
+			t.is(await keyv.get("foo"), undefined);
+		}
+	);
 
-  test.serial(".delete(key) resolves to true", async t => {
-    const keyv = new Keyv({ store: store() });
-    await keyv.set("foo", "bar");
-    t.is(await keyv.delete("foo"), true);
-  });
+	test.serial(".delete(key) resolves to true", async t => {
+		const keyv = new Keyv({ store: store() });
+		await keyv.set("foo", "bar");
+		t.is(await keyv.delete("foo"), true);
+	});
 
-  test.serial(
-    ".delete(key) with nonexistent key resolves to false",
-    async t => {
-      const keyv = new Keyv({ store: store() });
-      t.is(await keyv.delete("foo"), false);
-    }
-  );
+	test.serial(
+		".delete(key) with nonexistent key resolves to false",
+		async t => {
+			const keyv = new Keyv({ store: store() });
+			t.is(await keyv.delete("foo"), false);
+		}
+	);
 
-  test.serial(".delete(key) deletes a key", async t => {
-    const keyv = new Keyv({ store: store() });
-    await keyv.set("foo", "bar");
-    t.is(await keyv.delete("foo"), true);
-    t.is(await keyv.get("foo"), undefined);
-  });
+	test.serial(".delete(key) deletes a key", async t => {
+		const keyv = new Keyv({ store: store() });
+		await keyv.set("foo", "bar");
+		t.is(await keyv.delete("foo"), true);
+		t.is(await keyv.get("foo"), undefined);
+	});
 
-  test.serial(".clear() resolves to undefined", async t => {
-    const keyv = new Keyv({ store: store() });
-    t.is(await keyv.clear(), undefined);
-    await keyv.set("foo", "bar");
-    t.is(await keyv.clear(), undefined);
-  });
+	test.serial(".clear() resolves to undefined", async t => {
+		const keyv = new Keyv({ store: store() });
+		t.is(await keyv.clear(), undefined);
+		await keyv.set("foo", "bar");
+		t.is(await keyv.clear(), undefined);
+	});
 
-  test.serial(".clear() deletes all key/value pairs", async t => {
-    const keyv = new Keyv({ store: store() });
-    await keyv.set("foo", "bar");
-    await keyv.set("fizz", "buzz");
-    await keyv.clear();
-    t.is(await keyv.get("foo"), undefined);
-    t.is(await keyv.get("fizz"), undefined);
-  });
+	test.serial(".clear() deletes all key/value pairs", async t => {
+		const keyv = new Keyv({ store: store() });
+		await keyv.set("foo", "bar");
+		await keyv.set("fizz", "buzz");
+		await keyv.clear();
+		t.is(await keyv.get("foo"), undefined);
+		t.is(await keyv.get("fizz"), undefined);
+	});
 
-  /*
+	/*
    * These tests cause ava to throw:
    *  `Promise returned by test never resolved`
    * If they run before any other test
   */
 
-  test.serial(".set(key, value) returns a Promise", t => {
-    const keyv = new Keyv({ store: store() });
-    t.true(keyv.set("foo", "bar") instanceof Promise);
-  });
+	test.serial(".set(key, value) returns a Promise", t => {
+		const keyv = new Keyv({ store: store() });
+		t.true(keyv.set("foo", "bar") instanceof Promise);
+	});
 
-  test.serial(".get(key) returns a Promise", t => {
-    const keyv = new Keyv({ store: store() });
-    t.true(keyv.get("foo") instanceof Promise);
-  });
+	test.serial(".get(key) returns a Promise", t => {
+		const keyv = new Keyv({ store: store() });
+		t.true(keyv.get("foo") instanceof Promise);
+	});
 
-  test.serial(".delete(key) returns a Promise", t => {
-    const keyv = new Keyv({ store: store() });
-    t.true(keyv.delete("foo") instanceof Promise);
-  });
+	test.serial(".delete(key) returns a Promise", t => {
+		const keyv = new Keyv({ store: store() });
+		t.true(keyv.delete("foo") instanceof Promise);
+	});
 
-  test.serial(".clear() returns a Promise", async t => {
-    const keyv = new Keyv({ store: store() });
-    const returnValue = keyv.clear();
-    t.true(returnValue instanceof Promise);
-    await returnValue;
-  });
-  /* **** */
+	test.serial(".clear() returns a Promise", async t => {
+		const keyv = new Keyv({ store: store() });
+		const returnValue = keyv.clear();
+		t.true(returnValue instanceof Promise);
+		await returnValue;
+	});
+	/* **** */
 
-  test.after.always(async () => {
-    const keyv = new Keyv({ store: store() });
-    await keyv.clear();
-  });
+	test.after.always(async () => {
+		const keyv = new Keyv({ store: store() });
+		await keyv.clear();
+	});
 };
 
 const customTests = (test, Keyv, store) => {
-  test.beforeEach(async () => {
-    const keyv = new Keyv({ store: store() });
-    await keyv.clear();
-  });
+	test.beforeEach(async () => {
+		const keyv = new Keyv({ store: store() });
+		await keyv.clear();
+	});
 
-  test.serial(
-    ".set(key, value) should not overwrite existing keys",
-    async t => {
-      const keyv = new KeyvPouchDB({ overwriteExisting: true });
-      await keyv.set("foo", "bar");
-      await keyv.set("foo", "foo");
-      t.is(await keyv.get("foo"), "foo");
-    }
-  );
+	test.serial(
+		".set(key, value) should not overwrite existing keys",
+		async t => {
+			const keyv = new KeyvPouchDB({ overwriteExisting: true });
+			await keyv.set("foo", "bar");
+			await keyv.set("foo", "foo");
+			t.is(await keyv.get("foo"), "foo");
+		}
+	);
 
-  test.serial("should use the correct pouchdb adapter", t => {
-    let keyv = new KeyvPouchDB("http://localhost:3000");
-    t.is(keyv._opts.pouchDB.adapter, "http");
-    keyv = new KeyvPouchDB("database.db");
-    t.is(keyv._opts.pouchDB.adapter, "websql");
-  });
+	test.serial("should use the correct pouchdb adapter", t => {
+		let keyv = new KeyvPouchDB("http://localhost:3000");
+		t.is(keyv._opts.pouchDB.adapter, "http");
+		keyv = new KeyvPouchDB("database.db");
+		t.is(keyv._opts.pouchDB.adapter, "websql");
+	});
 
-  test.serial("should throw if pouchdb adapter is not supported", t => {
-    const error = t.throws(
-      () => new KeyvPouchDB({ pouchDB: { adapter: "none" } }),
-      Error
-    );
-    t.is(error.message, "Unsupported pouchdb adapter none");
-  });
+	test.serial("should throw if pouchdb adapter is not supported", t => {
+		const error = t.throws(
+			() => new KeyvPouchDB({ pouchDB: { adapter: "none" } }),
+			Error
+		);
+		t.is(error.message, "Unsupported pouchdb adapter none");
+	});
 
-  test.after.always(async () => {
-    const keyv = new Keyv({ store: store() });
-    await keyv.clear();
-  });
+	test.after.always(async () => {
+		const keyv = new Keyv({ store: store() });
+		await keyv.clear();
+	});
 };
 
 keyvValueTests(test, Keyv, store);
